cfee25db7394902fc2482b0534f3cabb71ef82e0
dzdn <dzdn@dell>
2018-03-25 22:22:38 +0800

    test:commit operation

--- src/main/java/cn/net/bigorange/eureka/App.java
+++ src/main/java/cn/net/bigorange/eureka/App.java
@@ -12,7 +12,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class App{
 	
     public static void main( String[] args ){
+    	
         SpringApplication.run(App.class, args);
+        
     }
     
 }
